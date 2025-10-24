0ea776aa58d12c6f6af5aa876e1861794555e7b4
Yana_Krasnopolskaya <krasnopolskaya.yana331@gmail.com>
2025-10-24 11:47:47 +0700

    added global styles, styles are set in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+    compatibilityDate: '2025-07-15',
+    devtools: { enabled: true },
+    css: [
+        '@/assets/styles/main.scss' // глобальный SCSS
+    ],
+
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: `@use "@/assets/styles/global/index" as *;`
+                },
+            },
+        },
+    },
 })
